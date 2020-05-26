d81df3ce0a47e888dc064c9c627f5a3aa0c23ade
darshan bordekar <darshanbordekar1819@gmail.com>
2020-05-26 11:32:53 +0530

    [ Darshan ] | Memory Leaks

--- Foundation P One/Memory Leaks.js
+++ Foundation P One/Memory Leaks.js
@@ -6,3 +6,4 @@ for (let index = 2; 1 < index; index++) {
 
 // Browser Will Crash.
 // When the data collection reaches its maximum level and there is no place to store further data called memory leak.
+// Filled Memory Heap With more and more and more data until website gets crashed
