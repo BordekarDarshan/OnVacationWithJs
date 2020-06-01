5bbc267a659ca40b123ac39cd46623a5b858af15
darshan bordekar <darshanbordekar1819@gmail.com>
2020-06-01 19:11:08 +0530

    [ Darshan ] | Garbage Collection

--- Foundation P One/Garbage Collection.js
+++ Foundation P One/Garbage Collection.js
@@ -7,3 +7,24 @@ let admin = user;
 user = null;
 
 console.log(admin);
+
+function marry(man, woman) {
+  man.wife = woman;
+  woman.husband = man;
+
+  return {
+    father: man,
+    mother: woman,
+  };
+}
+
+let family = marry(
+  {
+    name: "John",
+  },
+  {
+    name: "Ann",
+  }
+);
+
+console.log(family);
