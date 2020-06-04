aa76e6d2686e2924cabde797b4a8d345b45a17ad
darshan bordekar <darshanbordekar1819@gmail.com>
2020-06-04 12:35:30 +0530

    [ Darshan ] | First Typescript Code

---
+++ Types/Typescript First Program/FirstProgram.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var sum = function (a, b) {
+    return a + b;
+};
+var result = sum(1, 5);
+console.log(result);
---
+++ Types/Typescript First Program/FirstProgram.ts
@@ -0,0 +1,8 @@
+export {};
+
+const sum = (a: number, b: number) => {
+  return a + b;
+};
+
+const result = sum(1, 5);
+console.log(result);
