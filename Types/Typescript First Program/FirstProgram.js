d5c1a4686105eb7224d054645b4502e6a07c916f
darshan bordekar <darshanbordekar1819@gmail.com>
2020-06-04 12:46:27 +0530

    [ Darshan ] | First Typescript Program

--- Types/Typescript First Program/FirstProgram.js
+++ Types/Typescript First Program/FirstProgram.js
@@ -1,7 +1,13 @@
 "use strict";
 exports.__esModule = true;
+
 var sum = function (a, b) {
-    return a + b;
+  return a + b;
 };
 var result = sum(1, 5);
 console.log(result);
+
+function timeSaver(msg) {
+  console.log(msg);
+}
+timeSaver("Now You Don't have to write [tsc 'Program name'] command everytime");
--- Types/Typescript First Program/FirstProgram.ts
+++ Types/Typescript First Program/FirstProgram.ts
@@ -3,6 +3,10 @@ export {};
 const sum = (a: number, b: number) => {
   return a + b;
 };
-
 const result = sum(1, 5);
 console.log(result);
+
+function timeSaver(msg: string) {
+  console.log(msg);
+}
+timeSaver("Now You Don't have to write [tsc 'Program name'] command everytime");
