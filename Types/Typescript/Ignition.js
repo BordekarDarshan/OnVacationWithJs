c10dd319d8445cf84c60127e1713e38829675e41
darshan bordekar <darshanbordekar1819@gmail.com>
2020-06-04 12:25:57 +0530

    [ Darshan ] | Initial

---
+++ Types/Typescript/Ignition.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+var sum = function (a, b) {
+    return a + b;
+};
+function demoSpread() {
+    var para = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        para[_i] = arguments[_i];
+    }
+    console.log(para);
+}
---
+++ Types/Typescript/Ignition.ts
@@ -0,0 +1,9 @@
+export {};
+
+const sum = (a, b) => {
+  return a + b;
+};
+
+function demoSpread(...para) {
+  console.log(para);
+}
