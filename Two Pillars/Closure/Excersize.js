c759ab8d74d0931f2a3704ab46ac9efe7811ef9f
darshan bordekar <darshanbordekar1819@gmail.com>
2020-06-10 13:32:57 +0530

    [ Darshan ] | Closure Exercise

--- Two Pillars/Closure/Excersize.js
+++ Two Pillars/Closure/Excersize.js
@@ -1,11 +1,12 @@
-let view = undefined;
+// [1]
+//Because Page Shouldn't have initialize more that 4 times.
+
 function initialize() {
   view = "Home Page";
   console.log("Page has been initialized");
 }
 initialize();
 initialize();
-console.log(view);
 
 // Closure Way
 const closureInitialized = () => {
@@ -20,3 +21,25 @@ const closureInitialized = () => {
 let closureMethod = closureInitialized();
 closureMethod();
 closureMethod();
+
+// [2]
+const array = [1, 2, 3, 4];
+for (var index = 0; index < array.length; index++) {
+  setTimeout(() => {
+    console.log("I am at index " + index);
+  }, 3);
+}
+
+for (let index = 0; index < array.length; index++) {
+  setTimeout(() => {
+    console.log("I am at index with let " + array[index]);
+  });
+}
+
+for (var index = 0; index < array.length; index++) {
+  (function (closureI) {
+    setTimeout(() => {
+      console.log("I am at index with IIFE " + array[closureI]);
+    });
+  })(index);
+}
