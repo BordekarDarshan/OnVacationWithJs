2243c64890d9d5aae5f3349288918253f86642ea
darshan bordekar <darshanbordekar1819@gmail.com>
2020-06-11 17:35:11 +0530

    [ Darshan ] | Excercise

--- Two Pillars/Excersize.js
+++ Two Pillars/Excersize.js
@@ -4,3 +4,12 @@ Date.prototype.getLastYear = function () {
 
 let lastYearIs = new Date().getLastYear();
 console.log(lastYearIs);
+
+Array.prototype.map = function () {
+  let array = [];
+  for (i = 0; i < this.length; i++) {
+    array.push(this[i] + " Number");
+  }
+  return array;
+};
+console.log([1, 2, 3, 4].map());
