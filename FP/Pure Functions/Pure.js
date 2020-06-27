3cb9433bd4061bbb01cbbb3e1739a2d6e7bc52a6
darshan bordekar <darshanbordekar1819@gmail.com>
2020-06-27 15:19:45 +0530

    [ Darshan ] | Pure Function

--- FP/Pure Functions/Pure.js
+++ FP/Pure Functions/Pure.js
@@ -1,2 +1,21 @@
 // Function has to return the same output give the same input.
 // Function cannot modify anything outside of itself. [No side - effects].
+
+// [2] No Side Effects.
+
+const globalArray = [1, 2, 3];
+
+function modifyArrayPure(arr) {
+  const array = [].concat(arr);
+  array.forEach((item) => array.push(42));
+  return array;
+}
+
+function multiplyPure(arr) {
+  return arr.map((item) => item * 2);
+}
+
+const modifyArrayOne = modifyArrayPure(globalArray);
+const modifyArrayTwo = multiplyPure(globalArray);
+
+console.log(globalArray, modifyArrayOne, modifyArrayTwo);
---
+++ FP/Pure Functions/PureTest.js
@@ -0,0 +1,7 @@
+// whether it is pure function or not.
+function demo() {
+  console.log("Hii");
+}
+
+// Yes. because console belong to global context.
+// Ṭhis Function modified something outside.
