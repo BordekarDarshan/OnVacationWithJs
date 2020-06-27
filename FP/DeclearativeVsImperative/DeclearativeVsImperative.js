e994f8359d12487b374a12bbe57ecbdb9c633145
darshan bordekar <darshanbordekar1819@gmail.com>
2020-06-27 18:06:18 +0530

    [ Darshan] | Declarative | Pure

---
+++ FP/DeclearativeVsImperative/DeclearativeVsImperative.js
@@ -0,0 +1,14 @@
+// Imperative
+// Tells the machine what to do and how to do it.
+for (let index = 0; index < 10; index++) {
+  const element = index;
+  console.log(element);
+}
+
+// Declearative
+// What to do and what should happen
+// here we don't tell iterator to increment by one.
+[1, 2, 3].map((item) => console.log(item));
+
+// "FP" helps us be more declarative
+// Focus - "What to do"
--- FP/Pure Functions/Pure.js
+++ FP/Pure Functions/Pure.js
@@ -1,6 +1,27 @@
 // Function has to return the same output give the same input.
 // Function cannot modify anything outside of itself. [No side - effects].
 
+// [1] input === output
+
+function add(numOne, numTwo) {
+  return numOne + numTwo;
+}
+add(2, 4);
+
+function multiply(num) {
+  return num * 2;
+}
+// Referential Transparancy
+
+let result = multiply(add(2, 4));
+let output = multiply(6);
+
+console.log("result", result, "output", output);
+// Referential Transparancy says that if i change add(2, 4) in  multiply(add(2, 4)) to "6",
+// will it have any effect on the program ?
+// No, because of this "input === output" no matter what is input if they
+// are the same its always going to give me the same output.
+
 // [2] No Side Effects.
 
 const globalArray = [1, 2, 3];
@@ -19,3 +40,20 @@ const modifyArrayOne = modifyArrayPure(globalArray);
 const modifyArrayTwo = multiplyPure(globalArray);
 
 console.log(globalArray, modifyArrayOne, modifyArrayTwo);
+
+// functions "add", "multiply" are pure functions because they are not
+// touching any of the outside world. No Side Effects.
+// plus input === output.
+
+// Note : The goal of the "F.P." is not to make everything pure functions.
+// The is to minimize the side effects.
+
+// Use Case :
+// Organize your code with there is a specific part that has side effects,
+// but when you have bug you know right away to go to that spot because
+// that's where the side effects are happening.
+// Rest of your code is just a pure functions.
+
+// Goal : isolate side-effects => DB and API calls. so your code becomes predictable
+// and easier to find bug
+// You can't make your project 100% pure.
