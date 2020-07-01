bbcd1ebfe84b09dc47ed5bfb01cc394ed7e23da4
darshan bordekar <darshanbordekar1819@gmail.com>
2020-07-02 01:13:10 +0530

    [ Darshan] | ES6 modules

--- Modules/ES6 Modules/ES6Modules.js
+++ Modules/ES6 Modules/ES6Modules.js
@@ -1,1 +1,16 @@
+let harry = "harry";
+let voldemort = "he who must not ne named";
 
+export function fightWithEs(oponent1, oponent2) {
+  let attack1 = Math.floor(Math.random() * oponent1.length);
+  let attack2 = Math.floor(Math.random() * oponent2.length);
+
+  return attack1 > attack2 ? `${oponent1} wins` : `${oponent2} wins`;
+}
+
+// Because of modules we have now reusable functions and code.
+// code becomes maintainable because of that.
+// we are able to separate concerns and divide up our program into smaller chunks.
+// avoided global namespace pollution.
+// we can now compose different modules to create new modules or functionality to programs.
+// now can use 3rd party packages [npm].
